6e4eff201ba59dba65a5abbb6905fbe5f730b56b
Khumar <khumarns@code.edu.az>
2024-03-10 21:18:05 +0400

    connected hotels page with database, created hotelsPartialView

--- StayAzerbaijan/Controllers/HotelsController.cs
+++ StayAzerbaijan/Controllers/HotelsController.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using StayAzerbaijan.DAL;
+using StayAzerbaijan.ViewModels;
 
 namespace StayAzerbaijan.Controllers
 {
-    public class HotelsController:Controller
+    public class HotelsController : Controller
     {
         private readonly ProductDbContext _context;
 
@@ -14,7 +17,26 @@ namespace StayAzerbaijan.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var hotels = _context.Hotels
+                .Include(h => h.HotelCategories)
+                .Take(3) 
+                .ToList();
+
+            var categories = _context.Categories.ToList();
+            var model = new HotelsVM { Hotels = hotels, Categories = categories };
+            return View(model);
         }
+
+        public IActionResult GetMoreHotels(int page = 1, int pageSize = 3)
+        {
+            var hotels = _context.Hotels
+                .Include(h => h.HotelCategories)
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            return PartialView("_HotelPartial", hotels);
+        }
+
     }
 }
--- StayAzerbaijan/ViewModels/HotelsVM.cs
+++ StayAzerbaijan/ViewModels/HotelsVM.cs
@@ -1,6 +1,11 @@
-﻿namespace StayAzerbaijan.ViewModels
+﻿using StayAzerbaijan.Entities;
+
+namespace StayAzerbaijan.ViewModels
 {
     public class HotelsVM
     {
+        public List<Hotel> Hotels { get; set; }
+        public List<Category>? Categories { get; set; }
+        public List<HotelCategory>? HotelsCategory { get; set; }
     }
 }
